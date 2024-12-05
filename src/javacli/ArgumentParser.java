7eb401699ef9b92fd30d0aea7ce581a0fb8dc044
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-05 03:09:46 +0200

    Added parsers

--- src/javacli/ArgumentParser.java
+++ src/javacli/ArgumentParser.java
@@ -4,6 +4,10 @@ import java.util.regex.Matcher;
 import java.util.ArrayList;
 
 public interface ArgumentParser {
+    Class parses();
+
+    Object parse(String arg) throws ArgumentParseException;
+
     static Object parseUsing(Class type, ArgumentParser[] parsers, String arg) throws ArgumentParseException {
         ArgumentParser typeParser = null;
         for (ArgumentParser parser : parsers) {
@@ -35,7 +39,173 @@ public interface ArgumentParser {
         return matches.toArray(strings);
     }
 
-    Class parses();
+    static ArgumentParser[] parsers() {
+        return new ArgumentParser[] {
+            new ArgumentParser() {
+                public Class parses() {
+                    return String.class;
+                }
 
-    Object parse(String arg) throws ArgumentParseException;
+                public Object parse(String arg) {
+                    return arg;
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Byte.class;
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Byte.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Class.forPrimitiveName("byte");
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Byte.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Short.class;
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Short.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Class.forPrimitiveName("short");
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Short.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Integer.class;
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Integer.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Class.forPrimitiveName("int");
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Integer.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Long.class;
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Long.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Class.forPrimitiveName("long");
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Long.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Float.class;
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Float.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Class.forPrimitiveName("float");
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Float.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Double.class;
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Double.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            },
+            new ArgumentParser() {
+                public Class parses() {
+                    return Class.forPrimitiveName("double");
+                }
+
+                public Object parse(String arg) throws ArgumentParseException {
+                    try {
+                        return Double.valueOf(arg);
+                    } catch (Exception exception) {
+                        throw new ArgumentParseException("Cannot parse '" + arg + "' as " + parses() + ".", exception);
+                    }
+                }
+            }
+        };
+    }
 }
--- src/test/Program.java
+++ src/test/Program.java
@@ -3,33 +3,7 @@ import java.util.Scanner;
 import javacli.*;
 
 public class Program {
-    private static CLI cli = new CLI(
-        Program.class,
-        new ArgumentParser[] {
-            new ArgumentParser() {
-                public Class parses() {
-                    return String.class;
-                }
-
-                public Object parse(String arg) {
-                    return arg;
-                }
-            },
-            new ArgumentParser() {
-                public Class parses() {
-                    return Float.class;
-                }
-
-                public Object parse(String arg) throws ArgumentParseException {
-                    try {
-                        return Float.valueOf(arg);
-                    } catch (Exception exception) {
-                        throw new ArgumentParseException("Cannot parse '" + arg + "' as Float", exception);
-                    }
-                }
-            }
-        }
-    );
+    private static CLI cli = new CLI(Program.class, ArgumentParser.parsers());
 
     @Command(Name="add")
     public static void add(Float a, Float b) {
